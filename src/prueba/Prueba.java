23670cc89a84d89b2f0a6315ef46b213f7fa5962
elsyc <00125717@uca.edu.sv>
2018-04-24 15:01:15 -0600

    Se agrego un prin infromaivo

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -17,6 +17,7 @@ public class Prueba {
     public static void main(String[] args) {
         System.out.println("Allá en la fuente habia un chorrito\nse hacia grande se hacia chiquito\nEstaba de mal humor\npobre chorrito tenía calor ");
         System.out.println("Hola mundo ");
+        //sd
     }
     
 }
